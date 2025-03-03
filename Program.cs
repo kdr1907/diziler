7ef2c3a0917bd98266fda05fe212237db94ccf56
Kadir <kdrokszs7@gmail.com>
2025-03-03 07:30:15 +0300

    f

--- Program.cs
+++ Program.cs
@@ -40,6 +40,51 @@
         }
 
         Console.WriteLine("Ortalama: " + toplam / diziUzunlugu);
+
+
+        int[] sayiDizisi = {23, 12, 4, 86, 72, 3, 11, 17};
+
+
+
+        //array methodları
+        Console.WriteLine("**** Sırasız Dizi *****");
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+        Console.WriteLine("**** Sıralı Dizi ****");
+        Array.Sort(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+        //Clear
+        Console.WriteLine("**** Array Clear *******");
+
+        // sayiDizisi elemanlarını kullanarak 2. index ten itibaren 2 tane elemanı 0'lar.
+        Array.Clear(sayiDizisi, 2, 2);
+
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+        //Reverse
+        Console.WriteLine("**** Array Reverse *");
+        Array.Reverse(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+        //IndexOf
+        Console.WriteLine("**** Array IndexOf");
+        Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+        //Resize
+        Console.WriteLine("**** Array Resize *****");
+        
+        Array.Resize<int>(ref sayiDizisi, 9);
+        sayiDizisi[8] = 99;
+        
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
     }
 
 }    
